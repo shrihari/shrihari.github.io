aad6a0d8b7bb118c44cf12432856c046e77c79dc
Shrihari <shrihari@freshdesk.com>
2017-03-02 01:01:17 +0530

    Ground floor added

--- js/main.js
+++ js/main.js
@@ -6,24 +6,28 @@ var content = [
   	color: 1,
   	seats: '5',
   	id: 'mercury',
+    floor: 1,
   },
   { 
   	title: 'Venus',
   	color: 1,
   	seats: '5',
   	id: 'venus',
+    floor: 1,
   },
   { 
   	title: 'Earth',
   	color: 1,
   	seats: '5',
   	id: 'earth',
+    floor: 1,
   },
   { 
   	title: 'Mars',
   	color: 1,
   	seats: '5',
   	id: 'mars',
+    floor: 1,
   },
   { 
   	title: 'Jupiter',
@@ -31,6 +35,7 @@ var content = [
   	seats: '12',
     tv: 1,
   	id: 'winterfell',
+    floor: 1,
   },
   { 
   	title: 'Saturn',
@@ -38,6 +43,7 @@ var content = [
   	seats: '12',
     tv: 1,
   	id: 'hardhome',
+    floor: 1,
   },
   { 
   	title: 'Sun',
@@ -45,6 +51,7 @@ var content = [
   	seats: '22',
     tv: 1,
   	id: 'sun',
+    floor: 1,
   },
 
 
@@ -55,24 +62,28 @@ var content = [
   	color: 2,
   	seats: '4',
   	id: 'krypton',
+    floor: 1,
   },
   { 
   	title: 'Asgard',
   	color: 2,
   	seats: '4',
   	id: 'asgard',
+    floor: 1,
   },
   { 
   	title: 'Vulcan',
   	color: 2,
   	seats: '3',
   	id: 'vulcan',
+    floor: 1,
   },
   { 
   	title: 'Cybertron',
   	color: 2,
   	seats: '4',
   	id: 'cybertron',
+    floor: 1,
   },
 
   { 
@@ -80,18 +91,21 @@ var content = [
   	color: 1,
   	seats: '4',
   	id: 'neptune',
+    floor: 1,
   },
   { 
   	title: 'Uranus',
   	color: 1,
   	seats: '4',
   	id: 'uranus',
+    floor: 1,
   },
   { 
   	title: 'Pluto',
   	color: 1,
   	seats: '4',
   	id: 'pluto',
+    floor: 1,
   },
 
   { 
@@ -99,12 +113,14 @@ var content = [
     color: 2,
     seats: '4',
     id: 'planetx',
+    floor: 1,
   },
   { 
     title: 'Miller\'s Planet',
     color: 2,
     seats: '4',
     id: 'millers',
+    floor: 1,
   },
   
   { 
@@ -113,6 +129,7 @@ var content = [
   	seats: '25',
     tv: 1,
   	id: 'matrix',
+    floor: 1,
   },
 
   { 
@@ -120,61 +137,71 @@ var content = [
     color: 2,
     seats: '5',
     id: 'tatooine',
+    floor: 1,
   },
   { 
     title: 'Naboo',
     color: 2,
     seats: '5',
     id: 'naboo',
+    floor: 1,
   },
   { 
-  	title: 'Jakku',
+  	title: 'Coruscant',
   	color: 2,
   	seats: '5',
-  	id: 'jakku',
+  	id: 'coruscant',
+    floor: 1,
   },
 
   { 
-  	title: 'GST Road',
+  	title: 'GST',
   	color: 6,
   	seats: '5',
-  	id: 'gstroad',
+  	id: 'gst',
+    floor: 1,
   },
   { 
   	title: 'OMR',
   	color: 6,
   	seats: '5',
   	id: 'omr',
+    floor: 1,
   },
   { 
   	title: 'ECR',
   	color: 6,
   	seats: '5',
   	id: 'ecr',
+    floor: 1,
   },
   { 
   	title: 'Mount Road',
   	color: 6,
   	seats: '4',
   	id: 'mountroad',
+    floor: 1,
   },
   { 
   	title: 'Greame\'s Road',
   	color: 6,
   	seats: '4',
   	id: 'greamesroad',
+    floor: 1,
   },
   { 
   	title: 'TTK Road',
   	color: 6,
   	seats: '4',
   	id: 'ttkroad',
+    floor: 1,
   },
   { 
     title: 'Cathedral Road',
     color: 6,
     seats: '4',
     id: 'cathedralroad',
+    floor: 1,
   },
 
   { 
@@ -182,24 +209,28 @@ var content = [
   	color: 7,
   	seats: '4',
   	id: 'lighthouse',
+    floor: 1,
   },
   { 
   	title: 'Napier Bridge',
   	color: 7,
   	seats: '4',
   	id: 'napierbridge',
+    floor: 1,
   },
   { 
   	title: 'Valluvar Kottam',
   	color: 7,
   	seats: '5',
   	id: 'valluvarkottam',
+    floor: 1,
   },
   { 
   	title: 'Theosophical Society',
   	color: 7,
   	seats: '6',
   	id: 'theosophicalsociety',
+    floor: 1,
   },
   { 
   	title: 'Kapaleeshwar Temple',
@@ -207,49 +238,57 @@ var content = [
   	seats: '10',
     tv: 1,
   	id: 'kapaleeshwartemple',
+    floor: 1,
   },
   { 
     title: 'Rippon Building',
     color: 7,
     seats: '4',
     id: 'ripponbuilding',
+    floor: 1,
   },
   { 
   	title: 'Fort St. George',
   	color: 7,
   	seats: '4',
   	id: 'fortstgeorge',
+    floor: 1,
   },
   { 
   	title: 'Chennai Central',
   	color: 7,
   	seats: '5',
   	id: 'chennaicentral',
+    floor: 1,
   },
   { 
   	title: 'Boat Club',
   	color: 7,
   	seats: '3',
   	id: 'boatclub',
+    floor: 1,
   },
   { 
   	title: 'Spencer Plaza',
   	color: 7,
   	seats: '6',
   	id: 'spencerplaza',
+    floor: 1,
   },
 
   { 
-  	title: 'Foreshore Estate',
+  	title: 'Santhome Beach',
   	color: 7,
   	seats: '3',
-  	id: 'foreshore',
+  	id: 'santhome',
+    floor: 1,
   },
   { 
-  	title: 'Broken Bridge',
+  	title: 'Golden Beach',
   	color: 7,
   	seats: '3',
-  	id: 'brokenbridge',
+  	id: 'golden',
+    floor: 1,
   },
   { 
   	title: 'Marina Beach',
@@ -257,6 +296,7 @@ var content = [
   	seats: '16',
     tv: 1,
   	id: 'gameofthrones',
+    floor: 1,
   },
   { 
   	title: 'Elliot\'s Beach',
@@ -264,30 +304,233 @@ var content = [
   	seats: '16',
     tv: 1,
   	id: 'houseofcards',
+    floor: 1,
   },
   { 
   	title: 'Kovalam Beach',
   	color: 7,
   	seats: '4',
   	id: 'kovalam',
+    floor: 1,
   },
   { 
-  	title: 'Mahabalipuram',
+  	title: 'Pebble Beach',
   	color: 7,
   	seats: '3',
-  	id: 'mahabalipuram',
+  	id: 'pebble',
+    floor: 1,
   },
   { 
     title: 'Amphitheater',
     color: 6,
-    seats: '40',
+    seats: 'a lot of people',
     id: 'amphitheater',
+    floor: 1,
   },
+
+
+
+  { 
+    title: 'Sao Gabriel',
+    color: 6,
+    seats: '5',
+    id: 'ship-1',
+    floor: 0,
+  },
+  { 
+    title: 'Santa Maria',
+    color: 6,
+    seats: '5',
+    id: 'ship-2',
+    floor: 0,
+  },
+  { 
+    title: 'Seawise Giant',
+    color: 6,
+    seats: '5',
+    id: 'ship-3',
+    floor: 0,
+  },
+  { 
+    title: 'Solar Sailor',
+    color: 6,
+    seats: '5',
+    id: 'ship-4',
+    floor: 0,
+  },
+  { 
+    title: 'Sea Shadow',
+    color: 6,
+    seats: '5',
+    id: 'ship-5',
+    floor: 0,
+  },
+  { 
+    title: 'Scorpion',
+    color: 6,
+    seats: '15',
+    id: 'ship-6',
+    floor: 0,
+  },
+  { 
+    title: 'Seraph',
+    color: 6,
+    seats: '5',
+    id: 'ship-7',
+    floor: 0,
+  },
+  { 
+    title: 'Seeadler',
+    color: 6,
+    seats: '5',
+    id: 'ship-8',
+    floor: 0,
+  },
+
+  { 
+    title: 'Flying Dutchmen',
+    color: 8,
+    seats: '5',
+    id: 'ship-9',
+    floor: 0,
+  },
+  { 
+    title: 'Black Pearl',
+    color: 8,
+    seats: '5',
+    id: 'ship-10',
+    floor: 0,
+  },
+  { 
+    title: 'Montana',
+    color: 8,
+    seats: '5',
+    id: 'ship-11',
+    floor: 0,
+  },
+  { 
+    title: 'Poseidon',
+    color: 8,
+    seats: '5',
+    id: 'ship-12',
+    floor: 0,
+  },
+  { 
+    title: 'Titanic',
+    color: 8,
+    seats: '10',
+    id: 'ship-13',
+    floor: 0,
+  },
+
+  { 
+    title: 'Admiral',
+    color: 7,
+    seats: '5',
+    id: 'ship-14',
+    floor: 0,
+  },
+  { 
+    title: 'Argo',
+    color: 7,
+    seats: '5',
+    id: 'ship-15',
+    floor: 0,
+  },
+  { 
+    title: 'Amethyst',
+    color: 7,
+    seats: '5',
+    id: 'ship-16',
+    floor: 0,
+  },
+  { 
+    title: 'Beagle',
+    color: 7,
+    seats: '20',
+    id: 'ship-17',
+    floor: 0,
+  },
+  { 
+    title: 'Batillus',
+    color: 7,
+    seats: '5',
+    id: 'ship-18',
+    floor: 0,
+  },
+  { 
+    title: 'Bismarck',
+    color: 7,
+    seats: '5',
+    id: 'ship-19',
+    floor: 0,
+  },
+  { 
+    title: 'Barzan',
+    color: 7,
+    seats: '5',
+    id: 'ship-20',
+    floor: 0,
+  },
+  { 
+    title: 'INS S-21',
+    color: 7,
+    seats: '5',
+    id: 'ship-21',
+    floor: 0,
+  },
+  { 
+    title: 'INS Chakra',
+    color: 7,
+    seats: '5',
+    id: 'ship-22',
+    floor: 0,
+  },
+  { 
+    title: 'INS Arihant',
+    color: 7,
+    seats: '5',
+    id: 'ship-23',
+    floor: 0,
+  },
+  { 
+    title: 'INS Viraat',
+    color: 7,
+    seats: '5',
+    id: 'ship-24',
+    floor: 0,
+  },
+  { 
+    title: 'Challenger',
+    color: 7,
+    seats: '5',
+    id: 'ship-25',
+    floor: 0,
+  },
+  { 
+    title: 'Color Magic',
+    color: 7,
+    seats: '5',
+    id: 'ship-26',
+    floor: 0,
+  },
+  { 
+    title: 'Cutty Sark',
+    color: 7,
+    seats: '5',
+    id: 'ship-27',
+    floor: 0,
+  },
+
+
 ];
 
 $(document).ready(function(){
 
 	$('#selected-room').hide();
+
+  var currentFloor = 1;
+  $('#ground-floor').hide();
 	
 	$('.ui.dropdown').dropdown();
 
@@ -307,7 +550,7 @@ $(document).ready(function(){
 			.addClass('color'+result['color'])
 			.show();
 
-		$('.room, .big-room').addClass('faded');
+		$('.room, .big-room, .lifts').addClass('faded');
 		$("#"+result['id']).removeClass('faded');
 
 		// console.log($("body").scrollLeft());
@@ -343,6 +586,33 @@ $(document).ready(function(){
 	    }, 300);
 	};
 
+  // Switch to Ground Floor
+  var switchToGroundFloor = function(){
+    currentFloor = 0;
+    $('#first-floor').fadeOut(200);
+    $('#ground-floor').fadeIn(200);
+    $('#first-floor-button').removeClass('active');
+    $('#ground-floor-button').addClass('active');
+  }
+
+  // Switch to First Floor
+  var switchToFirstFloor = function(){
+    currentFloor = 1;
+    $('#ground-floor').fadeOut(200);
+    $('#first-floor').fadeIn(200);
+    $('#ground-floor-button').removeClass('active');
+    $('#first-floor-button').addClass('active');
+  }
+
+  // Switch to Floors
+  var switchFloors = function(){
+    if(currentFloor == 1)
+      switchToGroundFloor();
+    else if(currentFloor == 0)
+      switchToFirstFloor();
+  }
+
+  // Drag to pan the map
 	$(function(){
 		var curDown = false,
 			curYPos = 0,
@@ -351,7 +621,7 @@ $(document).ready(function(){
 				if(curDown === true){
 					$(window).scrollTop($(window).scrollTop() + (curYPos - m.pageY)); 
 					$(window).scrollLeft($(window).scrollLeft() + (curXPos - m.pageX));
-          $('#mapcontainer').addClass('noclick');
+          $('.mapcontainer').addClass('noclick');
 				}
 			});
 
@@ -373,32 +643,41 @@ $(document).ready(function(){
 	// 		);
 	// });	
 
+  // On Clicking a meeting room
 	$('.room, .big-room').click(function(){
 		var id = $(this).attr('id');
 		showRoom(id);
     return false;
 	});
 
-	$('.meeting-room').click(function(){
-		var id = $(this).attr('name');
-		showRoom(id);
-    return false;
-	});
+  // On Clicking a meeting room name in "Upcoming Meetings"
+  // $('.meeting-room').click(function(){
+  //   var id = $(this).attr('name');
+  //   showRoom(id);
+  //   return false;
+  // });
 
+  // On Search
 	$('.ui.search').search({
 		source: content,
     selectFirstResult: true,
 		onSelect: function(result, response) {
+      if(result['floor'] == 0) {
+        switchToGroundFloor();
+      } else if(result['floor'] == 1) {
+        switchToFirstFloor();
+      }
 			showRoom(result['id']);
 		}
 	});
 
-	$('#close-room, #mapcontainer').click(function(){
-    if ($('#mapcontainer').hasClass('noclick')) {
-      $('#mapcontainer').removeClass('noclick');
+  // Deselecting a Selected Room
+	$('#close-room, .mapcontainer').click(function(){
+    if ($('.mapcontainer').hasClass('noclick')) {
+      $('.mapcontainer').removeClass('noclick');
     }
     else {
-      $('.room, .big-room').removeClass('faded');
+      $('.room, .big-room, .lifts').removeClass('faded');
       $('#selected-room')
         .removeClass()
         .hide();
@@ -408,15 +687,29 @@ $(document).ready(function(){
 	});
 
   var scale = 0.8;
-  $('#mapcontainer').css('transform', 'scale('+scale+')');
+  $('.mapcontainer').css('transform', 'scale('+scale+')');
 
+  // Zoom Logic
   $('#zoom-in').click(function(){
-      scale = scale + 0.04  ;
-    $('#mapcontainer').css('transform', 'scale('+scale+')');
+    scale = scale + 0.04 ;
+    $('.mapcontainer').css('transform', 'scale('+scale+')');
   });
   $('#zoom-out').click(function(){
-      scale = scale - 0.04  ;
-    $('#mapcontainer').css('transform', 'scale('+scale+')');
+    scale = scale - 0.04 ;
+    $('.mapcontainer').css('transform', 'scale('+scale+')');
+  });
+
+  // Floor switch Logic
+  $('#first-floor-button').click(function(){
+    switchToFirstFloor();
+  });
+  $('#ground-floor-button').click(function(){
+    switchToGroundFloor();
+  });
+
+  // Lifts Floor Switch
+  $('.lifts').click(function(){
+    switchFloors();
   });
 });
 
